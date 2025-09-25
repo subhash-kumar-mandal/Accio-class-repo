765b07cde5d16dcba6d4d8ba1d8cba2729795b1f
subhash kumar mandal <pm817405@gmail.com>
2025-09-25 21:15:37 +0530

    question 1

---
+++ basic-questions/index.js
@@ -0,0 +1,32 @@
+// ------------------
+// Basics question 
+// ------------------
+
+ 
+// Question 1  Take two numbers a,b and print the sum, difference, product, quotient and modulus of a & b.
+// Example:
+// Input:
+// 10
+// 3
+// Output:
+// 13
+// 7
+// 30
+// 3.3333333333333335
+// 1
+
+
+let a = 10;
+let b = 3;
+
+ let sum  = a+b ;
+  let diff = a-b ;
+  let mulit = a*b ;
+  let  div  = a/b ;
+  let rem  = a%b ;
+  
+  console.log(sum)
+  console.log(diff)
+  console.log(mulit)
+  console.log(div)
+  console.log(rem)
