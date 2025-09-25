a352be8aa3f795d78f14c4a3e4235a2a0eee73fd
subhash kumar mandal <pm817405@gmail.com>
2025-09-25 21:29:20 +0530

    question 2

--- basic-questions/index.js
+++ basic-questions/index.js
@@ -25,8 +25,20 @@ let b = 3;
   let  div  = a/b ;
   let rem  = a%b ;
   
-  console.log(sum)
-  console.log(diff)
-  console.log(mulit)
-  console.log(div)
-  console.log(rem)
+//   console.log(sum)
+//   console.log(diff)
+//   console.log(mulit)
+//   console.log(div)
+//   console.log(rem)
+
+
+  
+// Question 2 Take two numbers and only print the integer part when a is divided by b .
+// input:
+// Two numbers - a,b
+// Output:
+// Integer part of the result a/b
+
+let num = 10; 
+let num1 = 4;
+// console.log(Math.floor(num/num1)) 
