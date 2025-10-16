e6f3e0284b6b28c46a051bb238055023d746c029
subhash-kumar-mandal <pm817405@gmail.com>
2025-10-16 20:14:18 +0530

    declarations and initialized

---
+++ shubham-sir/day-1/index.js
@@ -0,0 +1,19 @@
+// --> Declaration  = variable create karna ;
+//            . let ,const , var,
+
+// ----> Initialisation =  variable ko  value dena 
+  
+let a; //declarations
+a=10  // initialized
+
+let b ; // declarations
+b = 20; // initialized
+
+// const c; [Note :declarations must be initialized] , [Declaration and initialized same time] 
+const c=20; // good  way this
+
+
+// ---->   let,const,var name   =   value give 
+//       |____________________|   |___________|
+//                 |                    |
+//            declaration           initialized
