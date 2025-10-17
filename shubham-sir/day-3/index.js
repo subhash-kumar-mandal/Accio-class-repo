fbf9ddc7221e3e9b3dc01dbad4d023c8a437e3f3
subhash-kumar-mandal <pm817405@gmail.com>
2025-10-17 20:18:36 +0530

    data type and type casting

--- shubham-sir/day-3/index.js
+++ shubham-sir/day-3/index.js
@@ -13,3 +13,21 @@
 // exmple 
 let find = 2 + 3 * 4 > 10 && true || false
 console.log(find)
+
+
+
+// Data type |------------->  * Primitive type                  *  Non primitive type
+//                          1.number (1.02,23,-10)               1 object
+//                          2. string ("",'',``)                 2 array 
+//                          3. boolean (true and false)          3 function 
+//                          4. undefined
+//                          5. null
+//                          6. symbool
+//                          7. bigInt
+             
+
+
+
+
+// type casting   --> 1. Implicit (type coession);         2.Explicit(type conversion)
+                    //   (jo js khud karta hai )            ( jo haam khud karte hai)
