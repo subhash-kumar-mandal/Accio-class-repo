2e139123ab7399fb6532b2e2bdfc28b50793290f
subhash-kumar-mandal <pm817405@gmail.com>
2025-10-17 19:34:52 +0530

    nested ternary

---
+++ shubham-sir/day-2/index.js
@@ -0,0 +1,23 @@
+// ----> ternary operator 
+                //  true hone par 
+                //  /-------------|
+        //   condition ?_____________ : ______________
+                // |____________________________|
+                //  false hone par 
+
+
+
+// 1. 
+let num = 18 
+ num>=18?console.log("adult"):console.log("child");
+
+
+
+// 2. nested ternary 
+
+
+let number =0;
+number===0?console.log("zero"):
+number%2===0?
+(number>0?console.log("even and +ve"):console.log("even and -ve")):
+(number>0?console.log("odd and +ve"):console.log("odd and -ve"));
