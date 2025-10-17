28ca90863d55022cfd74adea291c5d40c5586496
subhash-kumar-mandal <pm817405@gmail.com>
2025-10-17 19:36:20 +0530

    js fix

--- shubham-sir/day-2/index.js
+++ shubham-sir/day-2/index.js
@@ -21,3 +21,13 @@ number===0?console.log("zero"):
 number%2===0?
 (number>0?console.log("even and +ve"):console.log("even and -ve")):
 (number>0?console.log("odd and +ve"):console.log("odd and -ve"));
+
+
+// 3. 
+let player = 1;
+let sides = "white";
+
+
+player===1?(sides==="white"?console.log("player1 is white side and player 2 is black"):console.log("player1 is black side and player 2 is white")):
+(sides==="white"?console.log("player 2 is white side and player1 is black side"):console.log("player 2 is black side and player1 is white side"))
+
