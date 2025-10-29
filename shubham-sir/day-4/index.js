4caa92346cde08132f04c43ee1f80c9eebdc22ae
subhash-kumar-mandal <pm817405@gmail.com>
2025-10-29 20:41:58 +0530

    fix js

---
+++ shubham-sir/day-4/index.js
@@ -0,0 +1,32 @@
+// loop topics  1. for loop
+//              2. whlie loop
+//              3. do while loop
+//              4. for of loop 
+//              5. for in loop
+
+// ---> while loop
+// *condition false hone par  loop break ho jata hai 
+ 
+// while(condition){
+//     code 
+
+// }
+
+
+// 1. print name 5 TimeRanges
+
+let i=1;
+while(i<=5){
+    console.log("hello");
+    i++  // i ko khud handal karna padta hai 
+}
+
+// 1  post increment   (phele value use karna phir update karna)   i--
+// 2  pre increment    (phele updated karna phir value use karna) ++i
+
+
+let name = prompt("entre your name");
+console.log(`i am ${name}`)
+
+
+// assigment question 
