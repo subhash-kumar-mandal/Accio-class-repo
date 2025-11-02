edad2dbae751ad035b4b7ce3b6aad01a40ef0567
subhash-kumar-mandal <pm817405@gmail.com>
2025-11-02 20:44:45 +0530

    patten

--- shubham-sir/day-7/index.js
+++ shubham-sir/day-7/index.js
@@ -9,3 +9,37 @@ let reverseangle= (n)=>{
 }
 
 // reverseangle(6)
+
+
+let rightangle_space =(n)=>{
+    for(let i=1;i<=n;i++){
+        let str ="";
+        for(let j=1;j<=n-i;j++){
+             str +=" "
+        }
+        for(let k=1;k<=i;k++){
+            str += "*"
+        }
+        console.log(str)
+    }
+}
+
+// rightangle_space(4)
+
+
+
+let squre = (n)=>{
+    for(let i=1;i<=n;i++){
+        let result = "";
+        for(let j=1;j<=n;j++){
+            if(i==1 ||i==n || j==1 ||j==n){
+                result +="* "
+            }else{
+                result +="  "
+            }
+        }
+        console.log(result)
+    }
+}
+
+squre(4)
