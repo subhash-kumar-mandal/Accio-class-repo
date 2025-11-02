4c1860f5b1aea7d8af5a8f0681a7b29b5df3cfd3
subhash-kumar-mandal <pm817405@gmail.com>
2025-11-02 21:54:30 +0530

    FIX JS AND HTML

--- shubham-sir/day-7/index.js
+++ shubham-sir/day-7/index.js
@@ -43,3 +43,60 @@ let squre = (n)=>{
 }
 
 squre(4)
+
+
+let leftangle =(num)=>{
+    for(let i=0;i<=num;i++){
+        let str ="";
+        for(let j=1;j<=i;j++){
+          str += "*"
+        }
+        console.log(str)
+    }
+}
+
+// leftangle(10)
+
+let spaceangle =(number)=>{
+    let o= number
+    for(let i=1;i<=number;i++){
+        let str ="";
+        for(let j=1;j<=number-i;j++){
+            str +=" "
+        }
+
+        for(let k=1;k<=i;k++){
+            if(i==number||k==1||k==i ){
+                str +="*";
+
+            }else{
+                str +=" "
+
+            }
+        }
+        console.log(str)
+    }
+}
+
+spaceangle(4)
+
+
+//  assigment question ;
+
+let star = (n)=>{
+    for(let i=1;i<=n;i++){
+        let str ="";
+        for(let j=1;j<=n-i;j++){
+            str += " "
+        };
+        for(let k=1;k<=i;k++){
+            str += "* ";
+
+        }
+        console.log(str)
+    }
+}
+
+star(7);
+
+
