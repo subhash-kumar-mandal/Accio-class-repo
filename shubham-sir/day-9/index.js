3df561ba2f0a267d5b41b3db8ee5fc88b748e470
subhash-kumar-mandal <pm817405@gmail.com>
2025-11-03 12:11:39 +0530

    fix js and html

---
+++ shubham-sir/day-9/index.js
@@ -0,0 +1,53 @@
+// Function  1.conventional function ;
+//           2. function expressioos 
+            //  3. Arrow  function
+
+//  syntax
+            // function name (para,para2){
+
+            //     // body 
+            // }
+function sayhi(){
+    console.log("hello guys")
+}
+
+sayhi()// invoke , call, run
+
+
+function user(name="entre name"){
+    console.log(name +" "+ "hello")
+}
+user()
+
+// 1. function definetion ;
+// 2. Parameters;
+// 3. Argurment ;
+// 4. function call , inoke ,run ;
+// 5. default param ;
+
+
+let hello = function(){
+    console.log("hello world")
+}
+
+hello()
+// fuction definetion  ek variable mein rakhna or usi se call karna ;
+
+
+
+let sum = (a,b)=>{
+ console.log(b+a)
+}
+
+sum(1,2)
+
+
+
+// function expressioos 
+
+// syntax ;               | 
+// let summ = function (a,b){
+//  console.log(b+a)
+// }
+
+// sum(1,2)  <---            
