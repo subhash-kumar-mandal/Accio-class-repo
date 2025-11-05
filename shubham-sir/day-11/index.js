c067193ed9d94319e5230739fc01e9fa744faecf
subhash-kumar-mandal <pm817405@gmail.com>
2025-11-05 20:53:47 +0530

    missing index

--- shubham-sir/day-11/index.js
+++ shubham-sir/day-11/index.js
@@ -1,3 +1,12 @@
 let arra =[1,2,4,6];
+let num =6
+function name1(arra,num){
+for(let i=0;i<arra.length;i++){
+    if(arra[i]>=num){
+        return i;
+    }
+}
+return arra.length
+}
 
-
+console.log(name1(arra,num))
