1a843c1f015bcd8da15a8ad07bfa471f6fc085a1
subhash-kumar-mandal <pm817405@gmail.com>
2025-11-12 20:50:08 +0530

    fix js

--- shubham-sir/day-14/index.js
+++ shubham-sir/day-14/index.js
@@ -1,3 +1,8 @@
+
+let new1=[4,5,0,-2,-3,1];
+let length = new1.length;
+let size = 5
+
 function subarrayDivisbleByK(arr,n,k){
  let ans =0;
   for(let i=0;i<arr.length;i++){
@@ -13,9 +18,11 @@ function subarrayDivisbleByK(arr,n,k){
 return ans
 }
 
+console.log(subarrayDivisbleByK(new1,length,size))
 
-
-
+let arr =[1,2,3,4,5,6,7,8,9,10];
+let window =3;
+let target =6
 
  function check(arr,n,s){
   for(let i=0;i<=arr.length - n;i++){
@@ -31,7 +38,7 @@ return ans
   }
   return "NO"
   }
-  console.log(check(arr,n,s))
+  console.log(check(arr,window,target))
 
 
   
