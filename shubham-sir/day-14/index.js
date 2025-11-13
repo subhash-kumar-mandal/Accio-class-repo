738c0ec22d663ee0b3581309dbc91231675d01d1
subhash-kumar-mandal <pm817405@gmail.com>
2025-11-13 20:34:23 +0530

    fix js

--- shubham-sir/day-14/index.js
+++ shubham-sir/day-14/index.js
@@ -1,74 +1,94 @@
 
-let new1=[4,5,0,-2,-3,1];
+let new1 = [4, 5, 0, -2, -3, 1];
 let length = new1.length;
 let size = 5
 
-function subarrayDivisbleByK(arr,n,k){
- let ans =0;
-  for(let i=0;i<arr.length;i++){
-    let sum =0;
-    for(let j=i;j<n;j++){
+function subarrayDivisbleByK(arr, n, k) {
+  let ans = 0;
+  for (let i = 0; i < arr.length; i++) {
+    let sum = 0;
+    for (let j = i; j < n; j++) {
       sum += arr[j]
-      if(sum% k==0){
-       ans++
+      if (sum % k == 0) {
+        ans++
       }
     }
-    
+
   }
-return ans
+  return ans
 }
 
-console.log(subarrayDivisbleByK(new1,length,size))
+console.log(subarrayDivisbleByK(new1, length, size))
 
-let arr =[1,2,3,4,5,6,7,8,9,10];
-let window =3;
-let target =6
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let window = 3;
+let target = 6
 
- function check(arr,n,s){
-  for(let i=0;i<=arr.length - n;i++){
-    let sum =0;
+function check(arr, n, s) {
+  for (let i = 0; i <= arr.length - n; i++) {
+    let sum = 0;
 
-    for(let j=i;j<i+n;j++){
-      sum+=arr[j]
+    for (let j = i; j < i + n; j++) {
+      sum += arr[j]
     }
-    if(sum===s){
-      return("YES")
+    if (sum === s) {
+      return ("YES")
     }
-    
+
   }
   return "NO"
-  }
-  console.log(check(arr,window,target))
+}
+console.log(check(arr, window, target))
 
 
-  
-let new_arra =[6,-4,3,2,-3]
+
+let new_arra = [6, -4, 3, 2, -3]
 let size1 = new_arra.length;
-  function splitArray (N, arr){
-	// Write your code here
+function splitArray(N, arr) {
+  // Write your code here
 
-	let sum=0;
-	for(let item of arr){
-		sum+=item 
-	}
+  let sum = 0;
+  for (let item of arr) {
+    sum += item
+  }
 
-	let temp= sum;
-	let sp =-1;
-	 
-	 for(let i=0;i<N;i++){
-		temp -=arr[i];
-		if(temp == (sum/2)){
-			sp=i	
-		}
-	 }
+  let temp = sum;
+  let sp = -1;
 
-	 if(sp== -1){
-		console.log("Not Possible")
-	 }else{
-		console.log(arr.slice(0,sp+1).join(" "))
-		console.log(arr.slice(sp+1,N).join(" "))
-	 }
-	
+  for (let i = 0; i < N; i++) {
+    temp -= arr[i];
+    if (temp == (sum / 2)) {
+      sp = i
+    }
+  }
+
+  if (sp == -1) {
+    console.log("Not Possible")
+  } else {
+    console.log(arr.slice(0, sp + 1).join(" "))
+    console.log(arr.slice(sp + 1, N).join(" "))
+  }
+
 }
 
-splitArray(size1,new_arra);
+splitArray(size1, new_arra);
+
+
+function zerosum(arra) {
+
+  for (let i = 0; i < arra.length; i++) {
+    let sum = 0;
+    for (let j = i; j < arra.length; j++) {
+      sum += arr[j];
+      if (sum == 0) {
+        flag = true;
+        console.log(`subaarya found from Index ${i} to ${j}`);
+
+      }
+    }
+  }
+  if (flag === false) {
+    console.log(-1);
+
+  }
+}
