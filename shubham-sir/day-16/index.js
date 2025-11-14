11381d57a003d1263eb127e49f7c23c2b0d267e8
subhash-kumar-mandal <pm817405@gmail.com>
2025-11-14 22:00:52 +0530

    js fix

--- shubham-sir/day-16/index.js
+++ shubham-sir/day-16/index.js
@@ -14,3 +14,25 @@ columwise(
         [7,8,9]
     ]
 )
+
+
+let columwise1=(arra)=>{
+    
+    for(let i=0;i<arra.length;i++){
+        let str ='';
+        for(let j=0;j<arra[i].length;j++){
+            str+=arra[j][i]+ " "
+        }
+        console.log(str)
+    }
+
+}
+
+columwise1(
+    [
+        [1,1,1,1],
+        [2,2,2,2],
+        [3,3,3,3],
+        [4,4,4,4]
+    ]
+)
