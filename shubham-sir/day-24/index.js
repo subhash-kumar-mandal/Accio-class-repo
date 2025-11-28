a9653e427cd698f0c6f3989111eb370d3f2d316d
subhash-kumar-mandal <pm817405@gmail.com>
2025-11-28 22:10:03 +0530

    compeleted

--- shubham-sir/day-24/index.js
+++ shubham-sir/day-24/index.js
@@ -6,3 +6,35 @@ function numbersum(n){
 }
 
 console.log(numbersum(5))
+
+function print(n){
+    if (n==0){
+        return;
+    }
+    print(n-1)
+    console.log("acciojob");
+
+}
+
+print(5)
+
+function mlt(n,m){
+    if (m==1){
+        return n;
+    }
+    return n+mlt(n,m-1);
+}
+
+console.log(mlt(10,13))
+
+
+function printXY(x, y) {
+ //Write your code here
+
+ if(y==x){
+  return  y;
+ }
+ return printXY(x,y-1)+` ${y}`;
+//  console.log(x)
+}
+console.log(printXY(15,20))
